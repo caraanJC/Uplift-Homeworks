0d476cb29c0540eec5f3589b4bfc38628c9b2c10
caraanJC <caraanjohncarlo@gmail.com>
2021-06-22 17:43:33 +0800

    feat: add isDuplicate() function

--- 012-js array and object/homework june 19, 2021/script.js
+++ 012-js array and object/homework june 19, 2021/script.js
@@ -1,16 +1,40 @@
 let tasks = ['eat', 'sleep', 'code'];
 
+// console.log tasks one by one
 const displayTasks = (tasks) => {
   for (let i = 0; i < tasks.length; i++) {
     console.log(`Task ${i + 1}: ${tasks[i]}`);
   }
 };
 
-const addTask = (task) => {
-  tasks.push(task);
+// Checks the array if it's already inside the tasks array
+const isDuplicate = (task) => {
+  return tasks.includes(task); // returns true or false
 };
 
+// Push task to tasks
+const addTask = (task) => {
+  // remove other data types beside string
+  if (typeof task !== 'string') {
+    return 'Enter a string';
+  }
+
+  // check for duplicates
+  if (isDuplicate(task)) {
+    return 'Already in tasks';
+  }
+
+  tasks.push(task);
+  console.log(`You added: ${task}`);
+  displayTasks(tasks);
+};
+
+// removes the last task
 const deleteTask = () => {
-  console.log(`You removed ${tasks.pop()}`);
+  // remove other data types beside string
+  if (typeof task !== 'string') {
+    return 'Enter a string';
+  }
+  console.log(`You removed: ${tasks.pop()}`);
   displayTasks(tasks);
 };
